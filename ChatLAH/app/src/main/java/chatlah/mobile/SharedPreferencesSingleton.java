262174d40b26139c2090b8b9c1cc1ea2abc395f5
Xin Zhe Khooi <khooi8913@gmail.com>
2018-09-29 18:58:09 +0800

    Updated SharedPreferencesSingleton

--- ChatLAH/app/src/main/java/chatlah/mobile/SharedPreferencesSingleton.java
+++ ChatLAH/app/src/main/java/chatlah/mobile/SharedPreferencesSingleton.java
@@ -1,4 +1,64 @@
 package chatlah.mobile;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class SharedPreferencesSingleton {
+
+    private static SharedPreferencesSingleton ourInstance;
+    private static Context mContext;
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+
+    private static String PACKAGE_NAME;
+    private static final String IS_LOGGED_IN = "IS_LOGGED_IN";
+
+    private SharedPreferencesSingleton(Context context) {
+        PACKAGE_NAME = context.getPackageName();
+        mContext = context.getApplicationContext();
+
+        sharedPreferences = context.getSharedPreferences(PACKAGE_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.apply();
+    }
+
+    public static SharedPreferencesSingleton getInstance(Context context){
+        if (ourInstance == null)    ourInstance = new SharedPreferencesSingleton(context);
+        return ourInstance;
+    }
+
+    public static String getSharedPrefStringVal(String sharedPrefKey){
+        return sharedPreferences.getString(sharedPrefKey, null);
+    }
+
+    public static void setSharedPrefStringVal(String sharedPrefKey, String value){
+        editor.putString(sharedPrefKey, value);
+        editor.apply();
+    }
+
+    public static boolean getSharedPrefBool(String sharedPrefKey){
+        return sharedPreferences.getBoolean(sharedPrefKey, false);
+    }
+
+    public static void setSharedPrefBool(String sharedPrefBool, boolean value){
+        editor.putBoolean(sharedPrefBool, value);
+        editor.apply();
+    }
+
+    public static float getSharedPrefFloat (String sharedPrefKey){
+        return sharedPreferences.getFloat(sharedPrefKey, 0);
+    }
+
+    public static void setSharedPrefFloat (String sharedPrefBool, float value){
+        editor.putFloat(sharedPrefBool, value);
+        editor.apply();
+    }
+
+    public static void clearSharedPrefs(){
+        editor.clear();
+        editor.apply();
+    }
+
 }
