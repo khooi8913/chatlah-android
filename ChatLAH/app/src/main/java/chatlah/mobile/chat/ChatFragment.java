b351f79ab2a52dcdebc4c8b331592196f2c9914e
Xin Zhe Khooi <khooi8913@gmail.com>
2018-10-02 21:42:05 +0800

    Added Chat & Info Fragment

--- ChatLAH/app/src/main/java/chatlah/mobile/chat/ChatFragment.java
+++ ChatLAH/app/src/main/java/chatlah/mobile/chat/ChatFragment.java
@@ -1,6 +1,42 @@
 package chatlah.mobile.chat;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chatlah.mobile.R;
 
 public class ChatFragment extends Fragment {
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getActivity().getApplicationContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        // Scroll here
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
