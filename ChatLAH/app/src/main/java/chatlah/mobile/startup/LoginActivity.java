4751d8353374d0f119b5d2a0c3c2fe464f676a6a
Xin Zhe Khooi <khooi8913@gmail.com>
2018-10-02 13:50:12 +0800

    Added Google SignIn UI

--- ChatLAH/app/src/main/java/chatlah/mobile/startup/LoginActivity.java
+++ ChatLAH/app/src/main/java/chatlah/mobile/startup/LoginActivity.java
@@ -1,5 +1,6 @@
 package chatlah.mobile.startup;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import chatlah.mobile.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mContext = this.getApplicationContext();
     }
 }
